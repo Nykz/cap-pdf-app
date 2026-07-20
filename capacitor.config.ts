d1b467ba974d3870e7c5e7d5fd95ca0c136bc5df
Nykz <nykz786@gmail.com>
2026-07-20 21:49:55 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.technyks.cappdfgenview',
+  appName: 'cap-pdf',
+  webDir: 'www'
+};
+
+export default config;
